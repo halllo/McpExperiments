110510c7172bef0c4570986e6d49445d17349505
Manuel Naujoks <mnaujoks@gmail.com>
2025-04-12 01:07:25 +0200

    sse transport with own httpclient

--- MyMCPClient/Program.cs
+++ MyMCPClient/Program.cs
@@ -1,61 +1,42 @@
 ﻿using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Logging;
-using ModelContextProtocol;
 using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol.Transport;
 using System.ClientModel;
+using System.Net.Http.Headers;
 
-
-
-
+List<McpClientTool> mcpTools = [];
 try
 {
-	var h = new HttpClient();
-	h.BaseAddress = new Uri("https://localhost:7148");
-	h.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("MyMCPClient", "1.0"));
-	//h.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyX25hbWUiLCJlbWFpbCI6InVzZXJfZW1haWwiLCJEYXRlT2ZKb2luaW5nIjoiMjAyMi0wOS0xMiIsImp0aSI6ImIyZmJiODVhLWIzYTctNDBlZC1hNzYwLWYyMDQ5OTlkM2U5MSIsImV4cCI6MTc0MzYzMzQ5OSwiaXNzIjoieW91cl9pc3N1ZXIiLCJhdWQiOiJ5b3VyX2F1ZGllbmNlIn0.z-tUleLMBg6fbjPLGqYYyZx_o6n98hpSCOVV7XPUPLg");
-	var c = await h.GetAsync("weatherforecast");
-	if (c.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+	await using var mcpClient1 = await McpClientFactory.CreateAsync(new StdioClientTransport(new()
 	{
-		var locationHeader = c.Headers.Location.AbsoluteUri;
+		Name = "Time MCP Server",
+		Command = @"..\..\..\..\MyMCPServer.Stdio\bin\Debug\net9.0\MyMCPServer.Stdio.exe",
+	}));
+	var mcpClient1Tools = await mcpClient1.ListToolsAsync();
+
+	var http = new HttpClient();
+	http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "my_key");//todo: acquire JWT
+	await using var mcpClient2 = await McpClientFactory.CreateAsync(new SseClientTransport(new()
+	{
+		Name = "Vibe MCP Server",
+		Endpoint = new Uri("https://localhost:7296/sse"),
+	}, http));
+	var mcpClient2Tools = await mcpClient2.ListToolsAsync();
+
+	mcpTools = mcpClient1Tools.Concat(mcpClient2Tools).ToList();
+	Console.WriteLine("Available MCP tools:");
+	foreach (var tool in mcpTools)
+	{
+		Console.WriteLine($"- {tool}");
 	}
-	c.EnsureSuccessStatusCode();
-	var s = await c.Content.ReadAsStringAsync();
-	Console.WriteLine(s);
+	Console.WriteLine();
 }
 catch (Exception e)
 {
+	Console.WriteLine((e.InnerException ?? e).Message);
 	Environment.Exit(1);
 }
-
-
-
-
-
-
-
-
-await using var mcpClient1 = await McpClientFactory.CreateAsync(new StdioClientTransport(new()
-{
-	Name = "Time MCP Server",
-	Command = @"..\..\..\..\MyMCPServer.Stdio\bin\Debug\net9.0\MyMCPServer.Stdio.exe",
-}));
-var mcpClient1Tools = await mcpClient1.ListToolsAsync();
-
-await using var mcpClient2 = await McpClientFactory.CreateAsync(new SseClientTransport(new()
-{
-	Name = "Vibe MCP Server",
-	Endpoint = new Uri("https://localhost:7296/sse"),
-}));
-var mcpClient2Tools = await mcpClient2.ListToolsAsync();
-
-var mcpTools = mcpClient1Tools.Concat(mcpClient2Tools).ToList();
-Console.WriteLine("Available MCP tools:");
-foreach (var tool in mcpTools)
-{
-	Console.WriteLine($"- {tool}");
-}
-Console.WriteLine();
 
 
 
