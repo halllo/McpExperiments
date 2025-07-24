fbae63764f200b55db955b75bf4fadd9a75775d6
Manuel Naujoks <mnaujoks@gmail.com>
2025-07-24 14:11:45 +0200

    cli as mcp

---
+++ MyMCPServer.Stdio.Cli/Program.cs
@@ -0,0 +1,77 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System.Reflection;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var host = CreateHostBuilder().Build();
+using (var serviceScope = host.Services.CreateScope())
+{
+	var serviceProvider = serviceScope.ServiceProvider;
+	try
+	{
+		var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+
+		var actions = typeof(Program).Assembly.GetTypes().Where(t => t.GetCustomAttribute<VerbAttribute>() != null).OrderBy(t => t.Name).ToArray();
+		var parserResult = Parser.Default.ParseArguments(args, actions);
+		var parsed = parserResult as Parsed<object>;
+
+		if (parsed != null)
+		{
+			var action = parsed.Value;
+			var actionInvocationMethod = action.GetType().GetMethods().Single(m => !m.IsSpecialName && !m.IsStatic && m.DeclaringType == action.GetType());
+			try
+			{
+				var methodArguments = actionInvocationMethod.GetParameters().Select(p => serviceProvider.GetRequiredService(p.ParameterType)).ToArray();
+				var result = actionInvocationMethod.Invoke(action, methodArguments);
+				if (result is Task t)
+				{
+					await t;
+				}
+			}
+			catch (TargetInvocationException e)
+			{
+				logger.LogError(e, "Cannot invoke command.");
+			}
+			catch (Exception e)
+			{
+				logger.LogError(e, "Unkown error.");
+			}
+			return 0;
+		}
+		else
+		{
+			return 1;
+		}
+	}
+	catch (Exception e)
+	{
+		var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+		logger.LogError(e, "Something went wrong.");
+		return 1;
+	}
+}
+
+static IHostBuilder CreateHostBuilder()
+{
+	return Host.CreateDefaultBuilder()
+		.ConfigureAppConfiguration(cfg =>
+		{
+			cfg.AddJsonFile("appsettings.local.json", optional: true);
+		})
+		.UseSerilog((ctx, cfg) =>
+		{
+			cfg.ReadFrom.Configuration(ctx.Configuration);
+		})
+		.ConfigureServices((ctx, services) =>
+		{
+			services
+				.AddMcpServer()
+				.WithStdioServerTransport()
+				.WithToolsFromAssembly()
+				;
+		});
+}
---
+++ MyMCPServer.Stdio.Cli/Verbs/NewGuid.cs
@@ -0,0 +1,25 @@
+﻿using CommandLine;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace MyMCPServer.Cli.Stdio.Verbs
+{
+	[Verb("guid")]
+	public class NewGuid
+	{
+		public async Task<string> Do(ILogger<NewGuid> logger)
+		{
+			var newGuid = Guid.NewGuid();
+			logger.LogInformation("Generated new GUID: {Guid}", newGuid);
+			return newGuid.ToString();
+		}
+	}
+
+	[McpServerToolType]
+	public class NewGuidMcp(ILogger<NewGuid> logger)
+	{
+		[McpServerTool, Description("Get a new guid.")]
+		public async Task<string> GetGuid() => await new NewGuid().Do(logger);
+	}
+}
